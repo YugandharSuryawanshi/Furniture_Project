4b4ca051fb741999cb256895ee8c67c889fe16be
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-02-18 17:04:36 +0530

    Added DB file

--- backend/connection.js
+++ backend/connection.js
@@ -3,11 +3,12 @@ import { config } from './config/config.js';
 import { promisify } from 'util';  // Ensure to import 'util' for promisify
 
 // Database connection setup
+// Here using or db names are given becouse anyone who create database
 const conn = mysql.createConnection({
-    host: config.db.host,
-    user: config.db.user,
-    password: config.db.password,
-    database: config.db.database
+    host: config.db.host || 'localhost',
+    user: config.db.user || 'root',
+    password: config.db.password || '',
+    database: config.db.database || 'furni_shop'
 });
 
 // Connect to the database
