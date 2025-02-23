7a560e89657b329d9254796123a9bc222e08e7a0
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-02-23 21:09:40 +0530

    Adding new component in admin module update-order due to change order status

--- frontend/furniProject/src/app/app.config.ts
+++ frontend/furniProject/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideHttpClient(),
     provideAnimations(),
-    provideToastr(),
+    provideToastr(), provideAnimationsAsync(), provideAnimationsAsync(),
   ]
 };
