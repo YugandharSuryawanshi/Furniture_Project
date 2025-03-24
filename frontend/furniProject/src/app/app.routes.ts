9c51579625b67efde8e1ea0e55fdf2a632eda475
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-03-24 11:05:28 +0530

    Update App Route

--- frontend/furniProject/src/app/app.routes.ts
+++ frontend/furniProject/src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
     { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
     { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
     { path: '', redirectTo: '/user/home', pathMatch: 'full' },
-    { path: '**', redirectTo: '/user/home', pathMatch: 'full' } // Fallback route
+    { path: '**', redirectTo: '/user/home', pathMatch: 'full' } // Fallback route Redirect User Home
 ];
 
 @NgModule({
